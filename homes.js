1c2f04612a2a48203b8a7546a5765912584cfe68
EmilyMacias <99148529+EmilyMacias@users.noreply.github.com>
2025-12-06 15:34:10 -0800

    plot new chart

---
+++ homes.js
@@ -0,0 +1,26 @@
+// define functions
+function plotChart(housing_data) {
+  console.log(processed_data);
+  const trace = {
+    x: processed_data.map((d) => d.date),
+    y: processed_data.map((d) => d.value),
+    type: "scatter",
+    mode: "lines",
+    name: "Median Housing Prices in the US",
+    line: { color: "steelblue" },
+  };
+
+  const layout = {
+    title: "Median Housing Prices in the US Over Time",
+    xaxis: { title: "Date" },
+    yaxis: { title: "Price in USD" },
+    autosize: true,
+  };
+
+  Plotly.newPlot("chart", [trace], layout);
+}
+
+// loading data and executing main logic
+d3.csv("housing_data.csv").then((data) => {
+  plotChart(data);
+});
